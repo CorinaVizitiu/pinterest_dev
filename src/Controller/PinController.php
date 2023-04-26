8b85169428fb7dd17f140ebefeec75e7204b76b7
CorinaVizitiu <corina.vizitiu@yahoo.com>
2023-04-26 08:55:52 +0200

    Ajout de createdAt et updatedAt dans mon entity PIN + migration en BD

--- src/Controller/PinController.php
+++ src/Controller/PinController.php
@@ -6,9 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Pin;
+
+
 class PinController extends AbstractController
 {
-    #[Route('/pin', name: 'app_pin')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         // return $this->render('pin/index.html.twig', [
@@ -17,6 +20,22 @@ class PinController extends AbstractController
 
         // return new Response("Hello World");
         // return $this->json(['message' => 'Hello World']);
+       
+        // $pin = new Pin;
+        // var_dump($pin);
+        // die;
+        
+        // $pin = new Pin;
+        // dump($pin);//(mieux que le var_dump, PRINCIPALEMENT UTILISER)
+        // die;
+
+        // $pin = new Pin;
+        // dd($pin); //(DUMP DIE, meilleur version du dump, plus besoin de mettre le die)
+
+
+        //  $pin = new Pin;
+        // dump($pin);//(tool bar)
+       
         return $this->render('pin/index.html.twig');
 
     }
